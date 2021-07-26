345ace50dca0eaefe810ee68807919ba625225b6
niksche <nikit.babe@gmail.com>
2021-07-26 18:17:43 +0300

    initial commit

---
+++ Server/index.js
@@ -0,0 +1,22 @@
+let fs = require('fs');
+let http = require('http');
+
+const server = http.createServer((req,res)=> {
+
+    const path = `.${req.url === '/' ? '/index.html' : req.url }`;
+
+    console.log('requested from ', req.url);
+    fs.readFile(path, (error, file) => {
+        if (error) {
+            res.write('error');
+            res.end;
+            return;
+        }
+        res.write(file);
+        res.end();
+        return;
+    });
+    
+});
+
+server.listen(3003);
